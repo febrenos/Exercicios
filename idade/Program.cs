34242d47e40d9ce9f541889366a03f4b51f60097
felipesugisawa <felipesugisawa1@gmail.com>
2019-09-23 17:30:23 -0300

    23/09

---
+++ idade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano   = 0;
+            int idade = 0;
+            do{
+                Console.Write("Em que dia você nasceu?");
+                ano = int.Parse(Console.ReadLine());
+                if((ano > 2021) || (ano < 0)){
+                    Console.WriteLine("data invalida");
+                }
+            } while((ano > 2021) || (ano < 0));
+
+            idade = 2019 - ano;
+
+            if(idade < 3) {
+                Console.WriteLine("você é: recem Nascido");
+            } else if((idade >= 3) && (idade <= 11)){
+                Console.WriteLine("você é: Criança");
+            } else if((idade >= 12) && (idade <= 19)){
+                Console.WriteLine("você é: Adolescente");
+            } else if((idade >= 20) && (idade <= 65)){
+                Console.WriteLine("você é: Adulto");
+            } else if(idade >= 65) {
+                Console.WriteLine("você é: Veinho");
+            } 
+
+
+        }
+    }
+}
---
+++ Áreas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcula Area");
+           Console.WriteLine("1 - quadrado"); 
+           Console.WriteLine("2 - triangulo");
+           Console.WriteLine("3 - circulo"); 
+           Console.WriteLine("4 - trapezio");
+           Console.WriteLine("5 - losango");
+           Console.WriteLine("6 - retangulo");
+           Console.Write("escolha uma opção: "); 
+           string Oper;
+           Opcao = Console.ReadLine();
+
+
+            switch(apcao){
+                case "1":
+                Console.WritwLine("Area do quadrado");
+                Console.write("Digite a base: ")
+                double basetri = double.Parse(Console.ReadLine());
+                Console.Write("Digite a Altura:");
+                double altTri = double.Parse(Console.ReadLine());
+                double areaTri = (basetri * altTRi) / 2;
+                Console.WriteLine("A area do triangulo é: " + areaTri);
+                break;
+            } 
+
+            case "2"
+                Console.WriteLine("area do Quadrado");
+                Console.Write("Qual o lado do quadrado? ");
+                double ladoQuad = double.Parse(Console.ReadLine());
+                double ladoQuad = ladoQuad * ladoQuad;
+                Console.WriteLine("A área do quadrado é: " + areaQuad);
+                break;
+            }
+
+
+        }
+    }
+}
