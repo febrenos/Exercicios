fd44cddc1dc8822789eb8f3c38dc8aed926b2ae7
felipesugisawa <felipesugisawa1@gmail.com>
2019-09-26 17:36:51 -0300

    matriz

---
+++ matriz/Program.cs
@@ -0,0 +1,60 @@
+﻿// O QUE EU FIZ DIA: 23/09
+// using System;
+// namespace matriz
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int[] vetor = new int[6];
+//             int impar = 0;
+//             int par = 0;
+//             for (int y = 0; y <= 5; y++)
+//             {
+//                 Console.Write("Digite o " + (y + 1) + "º número: ");
+//                 vetor[y] = int.Parse(Console.ReadLine());
+//                 if (vetor[y] % 2 != 0)
+//                 {
+//                     par += 1;
+//                 }
+//                 else
+//                 {
+//                     impar += 1;
+//                 }
+//             }
+//             Console.WriteLine(impar + " Números ímpares");
+//             Console.WriteLine(par + " Números pares");
+//         }
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// como o professor fez:
+using System;
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int impar = 0;
+            int par = 0;
+            
+            int[] vetor = new int [6];
+            for (int cont = 0; cont < 6; cont++){
+                Console.Write($"Digite o {cont} número: ");
+                vetor[cont] = int.Parse(Console.ReadLine());
+           }
+
+            foreach(int num in vetor){
+                if(num % 2 == 0){
+                    Par++;
+                } else {
+                    impar++;
+                }
+            }
+            Console.WriteLine($"Você tem {par} números pares e {impar} números ímpares");
+        }
+    }
+}
---
+++ matriz/tempmedia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace TempMedia
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           double maior = 0;
+           double menor = 0;
+           double[] temperaturas = new double[12];
+           for(int i = 0; i < 12; i++)
+           {
+               Console.WriteLine($"Digite a temperatura do mês {i+1}: ");
+               temperaturas[i]=double.Parse(Console.ReadLine());
+           }
+           maior = temperaturas[0];
+           menor = temperaturas[0];
+           foreach(double temp in temperaturas)
+           {
+               if(temp > maior){
+                   maior = temp;
+               } else if(temp < menor){
+                   menor = temp;
+               }
+           }
+           Console.WriteLine($"A maior temperatura é {maior}");
+           Console.WriteLine($"A menor temperatura é {menor}");
+       }
+   }
+}
